3fbe1f253ddc489c8ff2ab44e014878e20a71f11
Yuliana Apaza <yuliana.apaza94@gmail.com>
2020-10-16 20:48:37 -0500

    Main test

--- sqs-ruc-data-extraction/lambda_function.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print(event)
-    print("Hello World !!!!")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
