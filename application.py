0907eb6ea4a4981a6718e46034d2936d30890d03
Yuliana Apaza <yuliana.apaza94@gmail.com>
2020-10-18 00:11:38 -0500

    Update SUNAT rows

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import psycopg2
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-LOG_FILE = '/tmp/sample-app.log'
+LOG_FILE = '/tmp/crawler-ruc.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -20,111 +20,17 @@ connection = psycopg2.connect(
 )
 
 welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE>
 <html>
-<head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
-  <style>
-  body {
-    color: #ffffff;
-    background-color: #E0E0E0;
-    font-family: Arial, sans-serif;
-    font-size:14px;
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: none;
-  }
-  body.blurry {
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: #fff 0px 0px 25px;
-  }
-  a {
-    color: #0188cc;
-  }
-  .textColumn, .linksColumn {
-    padding: 2em;
-  }
-  .textColumn {
-    position: absolute;
-    top: 0px;
-    right: 50%;
-    bottom: 0px;
-    left: 0px;
-
-    text-align: right;
-    padding-top: 11em;
-    background-color: #1BA86D;
-    background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
-    background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
-  }
-  .textColumn p {
-    width: 75%;
-    float:right;
-  }
-  .linksColumn {
-    position: absolute;
-    top:0px;
-    right: 0px;
-    bottom: 0px;
-    left: 50%;
-
-    background-color: #E0E0E0;
-  }
-
-  h1 {
-    font-size: 500%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  h2 {
-    font-size: 200%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  ul {
-    padding-left: 1em;
-    margin: 0px;
-  }
-  li {
-    margin: 1em 0em;
-  }
-  </style>
-</head>
-<body id="sample">
-  <div class="textColumn">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Welcome</title>
+  </head>
+  <body>
     <h1>Congratulations</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
-  </div>
-  
-  <div class="linksColumn"> 
-    <h2>What's Next?</h2>
-    <ul>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_django.html">Deploy a Django Application to AWS Elastic Beanstalk</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_flask.html">Deploy a Flask Application to AWS Elastic Beanstalk</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_custom_container.html">Customizing and Configuring a Python Container</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/using-features.loggingS3.title.html">Working with Logs</a></li>
-
-    </ul>
-  </div>
-</body>
+  </body>
 </html>
 """
 
@@ -152,13 +58,23 @@ def crawl_ruc(ruc):
   if len(ruc) == 11:
     r = requests.get('https://api.sunat.cloud/ruc/' + ruc)
     if r.status_code == 200 and len(r.text) > 0:
-      cursor = connection.cursor()
-      postgres_insert_query = """ INSERT INTO info_sources_entityinformation (ruc, data, link, source) VALUES (%s,%s,%s,%s)"""
-      record_to_insert = (ruc, r.text, "","1")
-      cursor.execute(postgres_insert_query, record_to_insert)
-
-      connection.commit()
+        cursor = connection.cursor()
+        postgres_select_query = """select * from info_sources_entityinformation where ruc = %s and source = %s"""
+        cursor.execute(postgres_select_query, (ruc, "1"))
+        records = cursor.fetchone()
+        if len(records) > 0:
+          id = records[0]
+          postgres_update_query = """update info_sources_entityinformation set data = %s where id = %s"""
+          cursor.execute(postgres_update_query, (r.text, id))
+          connection.commit()
+          logger.info('[SUNAT] Se actualiza un registro para el RUC {}'.format(ruc))
+        else:
+          postgres_insert_query = """ INSERT INTO info_sources_entityinformation (ruc, data, link, source) VALUES (%s,%s,%s,%s)"""
+          record_to_insert = (ruc, r.text, "","1")
+          cursor.execute(postgres_insert_query, record_to_insert)
+          connection.commit()
+          logger.info('[SUNAT] Se crea un nuevo registro para el RUC {}'.format(ruc))
     else:
-      logger.error('No hay informacion para el RUC {}'.format(ruc))
+      logger.error('[SUNAT] No hay informacion para el RUC {}'.format(ruc))
   else:
-    logger.error('RUC {} no tiene 11 digitos'.format(ruc))
+    logger.error('[SUNAT] RUC {} no tiene 11 digitos'.format(ruc))
